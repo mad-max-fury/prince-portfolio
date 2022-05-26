97dc9659ac37e9c54914a383c44a95f1059b0266
mad-max-fury <chrisnonso8@gmail.com>
2022-05-26 11:55:39 +0100

    updated blog page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -37,7 +37,7 @@ const BlogPage = () => {
         <Main>
           <CurrentPostWrapper>
             <ImageWrap>
-              <img />
+              <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331" />
             </ImageWrap>
             <p>11th March 2022</p>
             <h5>
@@ -73,9 +73,28 @@ const CurrentPostWrapper = styled.div`
   width: 50%;
   height: 75vh;
   border: 1px solid red;
+
+  & > p:first-of-type {
+    font-size: 1.4rem;
+    font-weight: 600;
+    color: #fff;
+    padding: 15px 0;
+    border: 1px solid red;
+    text-align: start;
+  }
 `;
 const OldPost = styled.aside`
   display: flex;
   flex-direction: column;
+  height: 100%;
+  gap: 1rem;
 `;
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  height: 60%;
+  width: 100%;
+  & > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
